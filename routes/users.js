974fe68833b6e25f67368929863160286470a4bb
Milo <dtqhuong1@cmcglobal.vn>
2023-11-19 10:37:57 +0700

    validate request by express validator

--- routes/users.js
+++ routes/users.js
@@ -1,4 +1,5 @@
 import express from "express";
+import { body, validationResult, query } from "express-validator";
 
 const router = express.Router();
 
@@ -6,10 +7,22 @@ router.get("/", (req, res) => {
   res.send("Get users");
 });
 
-router.post("/login", (req, res) => {
-  debugger;
-  res.send("Login user");
-});
+router.post(
+  "/login",
+  body("email")
+    .trim()
+    .isEmail()
+    .custom(async (value) => {
+      if (value == "milo") throw new Error("Email is milo");
+    }),
+  (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+    res.send("Login user");
+  }
+);
 
 router.post("/register", (req, res) => {
   res.send("Register");
