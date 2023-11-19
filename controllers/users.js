305efc5534a1bda9225d283fc148bfc42109b4ab
Milo <dtqhuong1@cmcglobal.vn>
2023-11-19 15:49:05 +0700

    add class status code

--- controllers/users.js
+++ controllers/users.js
@@ -1,11 +1,12 @@
 import { validationResult } from "express-validator";
+import HttpStatusCode from "../exceptions/HttpStatusCode.js";
 
 const login = (req, res) => {
   const errors = validationResult(req);
   if (!errors.isEmpty()) {
-    return res.status(400).json({ errors: errors.array() });
+    return res.status(HttpStatusCode.BAD_REQUEST).json({ errors: errors.array() });
   }
-  res.status(200).json({ email: req.body.email });
+  res.status(HttpStatusCode.OK).json({ email: req.body.email });
 };
 
 export default { login };
