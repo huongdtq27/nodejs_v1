e700ee73df2779404b25a0680c0f8e1d0844d9d7
Milo <dtqhuong1@cmcglobal.vn>
2023-11-19 16:57:19 +0700

    add class exception

---
+++ exceptions/Exception.js
@@ -0,0 +1,13 @@
+export default class Exception extends Error {
+  static WRONG_DB_USERNAME_PASSWORD = "Wrong username and password";
+  static WRONG_CONNECTION_STRING = "Wrong server name/connection string";
+  static CANNOT_CONNECT_MONGODB = "Cannot connect to Mongoose";
+  static USER_EXIST = "User already exists";
+  static CANNOT_REGISTER_USER = "Cannot register user";
+  static WRONG_EMAIL_AND_PASSWORD = "Wrong email and password";
+
+  constructor(message, validationErrors = {}) {
+    super(message); //call constructor of parent class(Error)
+    this.validationErrors = validationErrors;
+  }
+}
